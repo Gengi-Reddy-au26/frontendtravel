812fa5443c83195216b2c82b62963a0cc4b572ae
Gangareddy N <gangareddyn17@gmail.com>
2023-02-03 13:26:47 +0530

    test

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,5 @@
-export const BASE_URL = BASE_URL = "http://localhost:4000/api/v1";
+export const BASE_URL = "https://travelworld-api.onrender.com"
+
+    //BASE_URL = "https://localhost:4000/api/v1";
 
     
